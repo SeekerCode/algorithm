fceab4e9b83e562028169ed7e559c1953851dc1a
unknown <SeekerYuanHG@163.com>
2020-10-07 08:01:52 +0800

    PostOrderTraverse

--- tree/BinaryTree/PostOrderTraverse/code.c
+++ tree/BinaryTree/PostOrderTraverse/code.c
@@ -11,6 +11,41 @@ typedef struct BiNode
     struct BiNode *lchild, *rchild;
 } BiNode, *BiTree;
 
+void PostOrderTraverse(BiTree T)
+{
+    //采用二叉链表存储结构,Visit是对数据元素操作的应用函数。
+    //后序遍历二叉树T的非递归算法,对每个数据元素调用函数Visit。
+    
+    InitStack(S);
+    BiTree p = T;
+    r = NULL;
+    while (p || !StackEmpty(S))
+    {
+        if (p) //走到最左边
+        {
+            push(S, p);
+            p = p->lchild;
+        }
+        else //向右
+        {
+            GetTop(S, p);                    //读取栈顶结点(不出栈)
+            if (p->rchild && p->rchild != r) //若右子树存在且未被访问过
+            {
+                p = p->rchild; //转向右
+                push(S, p);    //压入栈
+                p = p->lchild; //再走到最左
+            }
+            else //否则弹出结点并访问
+            {
+                pop(S, p);      //将结点弹出
+                visit(p->data); //访问
+                r = p;          //记录最近访问过的结点
+                p = NULL;       //访问完结点后重置p指针
+            }
+        }
+    }
+}
+
 void PostOrder(BiTree T)
 {
     if (T != NULL)
